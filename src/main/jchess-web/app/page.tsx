8465c169cd6f139cf2744026a8488e43ee053fcc
martinmarth <manni998.mw@gmail.com>
2023-11-25 17:22:25 +0100

    integrated navbar into main page

--- src/main/jchess-web/app/page.tsx
+++ src/main/jchess-web/app/page.tsx
@@ -1,8 +1,17 @@
+import { NewGameModal } from "@/components/NewGameModal";
+import Nav from "@/components/Nav";
 type Props = {
     searchParams: Record<string, string> | null | undefined;
 };
 export default function Home({ searchParams }: Props) {
     const showNewGameModal = searchParams?.newGame;
-    return <main className="h-screen bg-accent">Hello World!</main>;
+    return (
+        <main className="h-screen bg-accent">
+            <Nav />
+            <div className="flex flex-col" />
+
+            {showNewGameModal && <NewGameModal />}
+        </main>
+    );
 }
 
