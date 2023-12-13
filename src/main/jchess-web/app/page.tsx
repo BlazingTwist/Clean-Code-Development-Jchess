ba356f3f2bd3f67cc5f4a8e1861608d9cd3d70e6
martinmarth <manni998.mw@gmail.com>
2023-12-13 13:28:03 +0100

    added in code documentation for page and layout

--- src/main/jchess-web/app/page.tsx
+++ src/main/jchess-web/app/page.tsx
@@ -5,6 +5,12 @@ import Body from "@/components/Body";
 type Props = {
     searchParams: Record<string, string> | null | undefined;
 };
+
+/**
+ * Home component represents the whole content of the application.
+ *
+ * @param {HomeProps} props - The properties for the Home component.
+ */
 export default function Home({ searchParams }: Props) {
     const showNewGameModal = searchParams?.newGame;
 
@@ -12,6 +18,7 @@ export default function Home({ searchParams }: Props) {
         <main className="min-h-screen bg-accent min-w-[400px] ">
             <Nav />
             <Body />
+            {/* Display the NewGameModal if specified in the search parameters. */}
             {showNewGameModal && <NewGameModal />}
         </main>
     );
