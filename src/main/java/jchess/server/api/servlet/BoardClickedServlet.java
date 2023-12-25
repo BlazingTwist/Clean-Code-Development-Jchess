b1c939f39bd5cbb62ac2c0ac62d065f30803bb50
BlazingTwist <blazingtwist1@gmail.com>
2023-12-25 09:14:45 +0100

    cherryPick HttpUtils and SessionUtils from 24-implement-pawn-promotion

--- src/main/java/jchess/server/api/servlet/BoardClickedServlet.java
+++ src/main/java/jchess/server/api/servlet/BoardClickedServlet.java
@@ -7,9 +7,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jchess.common.IChessGame;
 import jchess.common.events.BoardClickedEvent;
 import jchess.ecs.EcsEvent;
+import jchess.server.GameSessionData;
+import jchess.server.util.HttpUtils;
 import jchess.server.util.JsonUtils;
 import jchess.server.util.SessionUtils;
 
@@ -19,9 +20,13 @@ public class BoardClickedServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         GameClicked clickInfo = JsonUtils.getMapper().readValue(req.getReader(), GameClicked.class);
-        IChessGame game = SessionUtils.findGame(clickInfo.getSessionId());
-        game.getEventManager().<EcsEvent<Vector2I>>getEvent(BoardClickedEvent.class).fire(clickInfo.getClickPos());
+        GameSessionData game = SessionUtils.findGame(clickInfo.getSessionId());
+        if (game == null) {
+            HttpUtils.error(resp, StatusCodes.NOT_FOUND, "Session does not exist");
+            return;
+        }
 
+        game.game.getEventManager().<EcsEvent<Vector2I>>getEvent(BoardClickedEvent.class).fire(clickInfo.getClickPos());
         resp.setStatus(StatusCodes.OK);
     }
 }
--- src/main/java/jchess/server/util/SessionUtils.java
+++ src/main/java/jchess/server/util/SessionUtils.java
@@ -1,14 +1,13 @@
 package jchess.server.util;
 
-import jchess.common.IChessGame;
 import jchess.server.GameSessionData;
 import jchess.server.session.SessionManager;
 import jchess.server.session.SessionMgrController;
 
 public class SessionUtils {
-    public static IChessGame findGame(String sessionId) {
+    public static GameSessionData findGame(String sessionId) {
         SessionManager<GameSessionData> gameManager = SessionMgrController.lookupSessionManager(GameSessionData.class);
         SessionManager.Session<GameSessionData> session = gameManager.getSession(sessionId);
-        return session.sessionData.game();
+        return session == null ? null : session.sessionData;
     }
 }
