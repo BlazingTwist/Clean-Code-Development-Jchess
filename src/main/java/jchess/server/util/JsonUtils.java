5bac02c056a0248b92cee11a04eb75b8549f7516
BlazingTwist <blazingtwist1@gmail.com>
2023-12-25 09:05:17 +0100

    cherryPick Traverse utility from 24-implement-pawn-promotion

--- src/main/java/jchess/server/util/JsonUtils.java
+++ src/main/java/jchess/server/util/JsonUtils.java
@@ -1,10 +1,45 @@
 package jchess.server.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JsonUtils {
+    private static final Logger logger = LoggerFactory.getLogger(JsonUtils.class);
+
     public static ObjectMapper getMapper() {
         // TODO erja, general serialization configuration here
         return new ObjectMapper();
+    }
+
+    public static <T> String serialize(T data) {
+        try {
+            return getMapper().writeValueAsString(data);
+        } catch (JsonProcessingException e) {
+            logger.error("Failed to serialize data.", e);
+            return "";
+        }
+    }
+
+    public static Traverse traverse(JsonNode node) {
+        return new Traverse(node);
+    }
+
+    public static final class Traverse {
+        private final JsonNode currentNode;
+
+        public Traverse(JsonNode currentNode) {
+            this.currentNode = currentNode;
+        }
+
+        public Traverse get(String propertyName) {
+            return currentNode == null ? this : new Traverse(currentNode.get(propertyName));
+        }
+
+        public String textValue() {
+            return currentNode == null ? null : currentNode.textValue();
+        }
     }
 }
